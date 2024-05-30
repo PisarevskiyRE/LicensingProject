c23f2755d7f43af374a4ce71b3fa99c1b4633276
PisarevskiyRE <daimon--@live.ru>
2024-05-30 23:10:55 +0300

    refactor

--- licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -1,20 +1,24 @@
 package com.optimagrowth.license.model;
 
-import org.springframework.hateoas.RepresentationModel;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 
-@Getter
-@Setter
-@ToString
+@Getter @Setter @ToString
 @Entity
-@Table(name = "license")
+@Table(name="licenses")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class License extends RepresentationModel<License> {
 
 	@Id
@@ -29,6 +33,15 @@ public class License extends RepresentationModel<License> {
 	private String licenseType;
 	@Column(name="comment")
 	private String comment;
+	@Transient
+	private String organizationName;
+	@Transient
+	private String contactName;
+	@Transient
+	private String contactPhone;
+	@Transient
+	private String contactEmail;
+
 
 	public License withComment(String comment){
 		this.setComment(comment);
