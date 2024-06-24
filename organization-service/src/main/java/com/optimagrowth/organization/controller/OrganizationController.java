a46d60594b2f58736c9e474e0f31ed14536acc5d
PisarevskiyRE <daimon--@live.ru>
2024-06-24 23:43:26 +0300

    chapter 10 in work

--- organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
+++ organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
@@ -1,11 +1,8 @@
 package com.optimagrowth.organization.controller;
 
-import javax.annotation.security.RolesAllowed;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,29 +20,27 @@ public class OrganizationController {
     @Autowired
     private OrganizationService service;
 
-    @RolesAllowed({ "ADMIN", "USER" })
+
     @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
     public ResponseEntity<Organization> getOrganization( @PathVariable("organizationId") String organizationId) {
         return ResponseEntity.ok(service.findById(organizationId));
     }
 
-    @RolesAllowed({ "ADMIN", "USER" })
     @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
     public void updateOrganization( @PathVariable("organizationId") String id, @RequestBody Organization organization) {
         service.update(organization);
     }
 
-    @RolesAllowed({ "ADMIN", "USER" })
     @PostMapping
     public ResponseEntity<Organization>  saveOrganization(@RequestBody Organization organization) {
         return ResponseEntity.ok(service.create(organization));
     }
 
-    @RolesAllowed("ADMIN")
-    @DeleteMapping(value="/{organizationId}")
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteLicense(@PathVariable("organizationId") String organizationId) {
-        service.delete(organizationId);
+    public void deleteOrganization( @PathVariable("organizationId") String organizationId) {
+        service.delete( organizationId );
+
     }
 
 }
--- organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
+++ organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.optimagrowth.organization.events.source.SimpleSourceBean;
 import com.optimagrowth.organization.model.Organization;
 import com.optimagrowth.organization.repository.OrganizationRepository;
 
@@ -19,24 +20,31 @@ public class OrganizationService {
     @Autowired
     private OrganizationRepository repository;
 
+    @Autowired
+    SimpleSourceBean simpleSourceBean;
+
     public Organization findById(String organizationId) {
         Optional<Organization> opt = repository.findById(organizationId);
+        simpleSourceBean.publishOrganizationChange("GET", organizationId);
         return (opt.isPresent()) ? opt.get() : null;
     }
 
     public Organization create(Organization organization){
         organization.setId( UUID.randomUUID().toString());
         organization = repository.save(organization);
+        simpleSourceBean.publishOrganizationChange("SAVE", organization.getId());
         return organization;
 
     }
 
     public void update(Organization organization){
         repository.save(organization);
+        simpleSourceBean.publishOrganizationChange("UPDATE", organization.getId());
     }
 
     public void delete(String organizationId){
         repository.deleteById(organizationId);
+        simpleSourceBean.publishOrganizationChange("DELETE", organizationId);
     }
 
     @SuppressWarnings("unused")
